aa8f514ce5a6a932adc23cd944666051783bfe92
Ofer Dagan <oferda4@gmail.com>
2022-04-01 16:36:04 +0300

    clang format

--- tester/test/tester_tests/ExceptionsTest.cpp
+++ tester/test/tester_tests/ExceptionsTest.cpp
@@ -9,18 +9,17 @@ static const wstring ARBITRARY_EXCEPTION_MESSAGE = L"arbitrary";
 static const uint32_t ARBITRARY_EXCEPTION_NUM = 8;
 
 TEST(Exceptions, ExceptionConstruction) {
-	Exception myException(ARBITRARY_EXCEPTION_MESSAGE, ARBITRARY_EXCEPTION_NUM);
-	EXPECT_EQ(ARBITRARY_EXCEPTION_MESSAGE, myException.viewMsg());
-	EXPECT_EQ(ARBITRARY_EXCEPTION_NUM, myException.getNum());
+    Exception myException(ARBITRARY_EXCEPTION_MESSAGE, ARBITRARY_EXCEPTION_NUM);
+    EXPECT_EQ(ARBITRARY_EXCEPTION_MESSAGE, myException.viewMsg());
+    EXPECT_EQ(ARBITRARY_EXCEPTION_NUM, myException.getNum());
 }
 
 TEST(Exceptions, WinAPIException) {
-	// TODO: Use guard / mock
-	const auto lastError = GetLastError();
-	SetLastError(ARBITRARY_EXCEPTION_NUM);
-	WinAPIException myException(ARBITRARY_EXCEPTION_MESSAGE);
-	EXPECT_EQ(ARBITRARY_EXCEPTION_MESSAGE, myException.viewMsg());
-	EXPECT_EQ(ARBITRARY_EXCEPTION_NUM, myException.getNum());
-	SetLastError(lastError);
-
+    // TODO: Use guard / mock
+    const auto lastError = GetLastError();
+    SetLastError(ARBITRARY_EXCEPTION_NUM);
+    WinAPIException myException(ARBITRARY_EXCEPTION_MESSAGE);
+    EXPECT_EQ(ARBITRARY_EXCEPTION_MESSAGE, myException.viewMsg());
+    EXPECT_EQ(ARBITRARY_EXCEPTION_NUM, myException.getNum());
+    SetLastError(lastError);
 }
