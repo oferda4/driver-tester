60d0cdda8dd968bf22a8049b4a597b7211138129
Ofer Dagan <oferda4@gmail.com>
2022-04-21 15:27:38 +0300

    Join service related classes

--- tester/src/tester/ServiceAPI.h
+++ tester/src/tester/ServiceAPI.h
@@ -32,28 +32,3 @@ struct WinServiceAPI {
     void stop(HandleType& handle);
     void remove(HandleType& handle);
 };
-
-template <typename T>
-concept ServiceManager = requires(T, const std::wstring& name, const std::wstring& pePath) {
-    { T::HandleType };
-    { T::create(name, pePath) } -> std::same_as<typename T::HandleType>;
-    { T::open(name) } -> std::same_as<typename T::HandleType>;
-};
-
-class SCManager final {
-public:
-    using HandleType = ServiceHandle;
-
-    static HandleType create(const std::wstring& name, const std::wstring& pePath);
-    static HandleType open(const std::wstring& name);
-
-private:
-    SCManager();
-    NOCOPY(SCManager);
-    MOVEABLE(SCManager);
-
-    HandleType createInternal(const std::wstring& name, const std::wstring& pePath);
-    HandleType openInternal(const std::wstring& name);
-
-    ServiceHandle m_handle;
-};
