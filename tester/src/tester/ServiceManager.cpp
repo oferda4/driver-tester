0316d371e5896a75e948869cb035f40117a2e4b8
Ofer Dagan <oferda4@gmail.com>
2022-04-21 14:47:02 +0300

    Change service manager interface

--- tester/src/tester/ServiceManager.cpp
+++ tester/src/tester/ServiceManager.cpp
@@ -7,6 +7,10 @@ SCManager::SCManager() : m_handle(OpenSCManager(nullptr, nullptr, SC_MANAGER_ALL
 }
 
 ServiceHandle SCManager::create(const std::wstring& name, const std::wstring& pePath) {
+    return SCManager().createInternal(name, pePath);
+}
+
+ServiceHandle SCManager::createInternal(const std::wstring& name, const std::wstring& pePath) {
     return CreateService(m_handle,
                          name.c_str(),
                          name.c_str(),
@@ -23,5 +27,9 @@ ServiceHandle SCManager::create(const std::wstring& name, const std::wstring& pe
 }
 
 ServiceHandle SCManager::open(const std::wstring& name) {
+    return SCManager().openInternal(name);
+}
+
+ServiceHandle SCManager::openInternal(const std::wstring& name) {
     return OpenService(m_handle, name.c_str(), SERVICE_ALL_ACCESS);
 }
--- tester/src/tester/ServiceManager.h
+++ tester/src/tester/ServiceManager.h
@@ -5,15 +5,27 @@
 #include "Service.h"
 #include "Handle.h"
 
+template <typename T>
+concept ServiceManager = requires(T, const std::wstring& name, const std::wstring& pePath) {
+    { T::HandleType };
+    { T::create(name, pePath) } -> std::same_as<typename T::HandleType>;
+    { T::open(name) } -> std::same_as<typename T::HandleType>;
+};
+
 class SCManager final {
 public:
-    SCManager();
+    using HandleType = ServiceHandle;
 
-    MOVEABLE(SCManager);
-
-    ServiceHandle create(const std::wstring& name, const std::wstring& pePath);
-    ServiceHandle open(const std::wstring& name);
+    static HandleType create(const std::wstring& name, const std::wstring& pePath);
+    static HandleType open(const std::wstring& name);
 
 private:
+    SCManager();
+    NOCOPY(SCManager);
+    MOVEABLE(SCManager);
+
+    HandleType createInternal(const std::wstring& name, const std::wstring& pePath);
+    HandleType openInternal(const std::wstring& name);
+
     ServiceHandle m_handle;
 };
