07406fc5b13020637ddbaaa6273a6b0d3cde8a18
Ofer Dagan <oferda4@gmail.com>
2022-09-29 14:52:37 +0300

    test: send interrupted in the middle of sending size

--- drvut_user/test/StreamTest.cpp
+++ drvut_user/test/StreamTest.cpp
@@ -15,13 +15,15 @@ constexpr uint32_t ARBITRARY_BUFFER_SIZE = 4096;
 constexpr uint32_t ARBITRARY_CHUNK_SIZE = 1024;
 static_assert(ARBITRARY_BUFFER_SIZE > ARBITRARY_CHUNK_SIZE);
 static_assert(ARBITRARY_BUFFER_SIZE % ARBITRARY_CHUNK_SIZE == 0);
+
+constexpr uint32_t SIZE_BUFFER_SIZE = static_cast<uint32_t>(sizeof(StreamImpl<MoveableMockConnection>::SizeType));
 }
 
 TEST(StreamTest, send) {
     MoveableMockConnection connection;
     EXPECT_CALL(connection.getMock(), send(_))
         .Times(ARBITRARY_BUFFER_SIZE / ARBITRARY_CHUNK_SIZE + 1)
-        .WillOnce(Return(static_cast<uint32_t>(sizeof(StreamImpl<MoveableMockConnection>::SizeType))))
+        .WillOnce(Return(SIZE_BUFFER_SIZE))
         .WillRepeatedly(Return(ARBITRARY_CHUNK_SIZE));
 
     StreamImpl stream(std::move(connection));
@@ -39,7 +41,16 @@ TEST(StreamTest, recv) {
     ASSERT_NO_THROW((void)stream.recv());
 }
 
-// TODO: test connection termination in the middle of send / recv
+TEST(StreamTest, sendInterruptedWhileSendingSize) {
+    MoveableMockConnection connection;
+    EXPECT_CALL(connection.getMock(), send(_))
+        .Times(2)
+        .WillOnce(Return(SIZE_BUFFER_SIZE - 1))
+        .WillRepeatedly(Return(0));
+
+    StreamImpl stream(std::move(connection));
+    ASSERT_THROW(stream.send(getFakeBuffer(ARBITRARY_BUFFER_SIZE)), ConnectionTerminatedInTheMiddle);
+}
 
 namespace {
 Buffer getFakeBuffer(size_t size) {
