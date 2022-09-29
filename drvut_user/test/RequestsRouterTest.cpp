c8145cc8e122e356bcb1872f2d632feae9832d1a
Ofer Dagan <oferda4@gmail.com>
2022-09-29 15:59:20 +0300

    test: rest of commands in router

--- drvut_user/test/RequestsRouterTest.cpp
+++ drvut_user/test/RequestsRouterTest.cpp
@@ -34,3 +34,49 @@ TEST(RequestsRouterTest, routingListFixtures) {
     RequestsRouterImpl router(std::move(handler), std::move(protocol));
     ASSERT_NO_THROW((void)router.route(fakeBuffer));
 }
+
+TEST(RequestsRouterTest, routingListTests) {
+    MoveableMockRequestsHandler handler;
+    MoveableMockProtocol protocol;
+    Buffer fakeBuffer;
+
+    EXPECT_CALL(protocol.getMock(), parse(_))
+        .WillOnce(
+            Return(Request(RequestType::LIST_TESTS, fakeBuffer)));
+    EXPECT_CALL(protocol.getMock(), parseListTestsOutput(_))
+        .WillOnce(
+            Return(fakeBuffer));
+    EXPECT_CALL(protocol.getMock(), parseListTestsInput(_))
+        .WillOnce(
+            Return(ListTestsInput()));
+
+    EXPECT_CALL(handler.getMock(), listTests(_))
+        .WillOnce(
+            Return(ListTestsOutput()));
+
+    RequestsRouterImpl router(std::move(handler), std::move(protocol));
+    ASSERT_NO_THROW((void)router.route(fakeBuffer));
+}
+
+TEST(RequestsRouterTest, routingRunTest) {
+    MoveableMockRequestsHandler handler;
+    MoveableMockProtocol protocol;
+    Buffer fakeBuffer;
+
+    EXPECT_CALL(protocol.getMock(), parse(_))
+        .WillOnce(
+            Return(Request(RequestType::RUN_TEST, fakeBuffer)));
+    EXPECT_CALL(protocol.getMock(), parseRunTestOutput(_))
+        .WillOnce(
+            Return(fakeBuffer));
+    EXPECT_CALL(protocol.getMock(), parseRunTestInput(_))
+        .WillOnce(
+            Return(RunTestInput()));
+
+    EXPECT_CALL(handler.getMock(), runTest(_))
+        .WillOnce(
+            Return(RunTestOutput()));
+
+    RequestsRouterImpl router(std::move(handler), std::move(protocol));
+    ASSERT_NO_THROW((void)router.route(fakeBuffer));
+}
