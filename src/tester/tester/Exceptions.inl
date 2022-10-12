7f1b2f8e2965e9b0a375ad3051476b5ebef704ea
oferda4 <oferda4@gmail.com>
2022-10-12 11:23:05 +0300

    chore: remove old user mode code

--- src/tester/tester/Exceptions.inl
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "Exceptions.h"
-
-template <LastErrorAPIImpl T>
-WinAPIException<T>::WinAPIException(std::wstring msg, const T& t) : 
-    Exception(std::move(msg), t.getLastError()) {
-    // Left blank intentionally
-}
--- test/tester_tests/SafetyTest.cpp
+++
@@ -1,9 +0,0 @@
-#include <gtest/gtest.h>
-
-#include "tester/Safety.h"
-#include "FakeException.h"
-#include "MockTracer.h"
-
-TEST(SafetyTest, TryExecuteThrowingFunc) {
-    ASSERT_NO_THROW(Safety::tryExecute<MockTracer>([] { throw FakeException(); }));
-}
