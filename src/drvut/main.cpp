3d6dabc6b20aaa2afc63ddd1009a59dd8c6f4d5e
oferda4 <oferda4@gmail.com>
2022-10-21 18:16:51 +0300

    feat: implement list fixtures and add stubs

--- src/drvut/main.cpp
+++ src/drvut/main.cpp
@@ -2,6 +2,7 @@
 
 #include "Device.h"
 #include "Fixture.h"
+#include "Ioctls.h"
 
 namespace drvut {
 namespace internal {
@@ -14,6 +15,9 @@ void unloadDriver(PDRIVER_OBJECT DriverObject);
 NTSTATUS doNothing(PDEVICE_OBJECT deviceObject, PIRP irp);
 NTSTATUS deviceControl(PDEVICE_OBJECT deviceObject, PIRP irp);
 NTSTATUS performIoctl(PIRP irp);
+NTSTATUS handleListFixturesIoctl(FixtureInfo* outFixtures, size_t outFixturesLength);
+NTSTATUS handleListTestsIoctl(PIRP irp);
+NTSTATUS handleRunTestIoctl(PIRP irp);
 }
 
 EXTERN_C NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath) {
@@ -76,15 +80,43 @@ NTSTATUS performIoctl(PIRP irp) {
     auto irpSp = IoGetCurrentIrpStackLocation(irp);
     const auto inBufferLength = irpSp->Parameters.DeviceIoControl.InputBufferLength;
     const auto outBufferLength = irpSp->Parameters.DeviceIoControl.OutputBufferLength;
-
+    
     if (!inBufferLength || !outBufferLength) {
         return STATUS_INVALID_PARAMETER;
     }
 
+    auto inBuffer = irp->AssociatedIrp.SystemBuffer;
+    auto outBuffer = irp->AssociatedIrp.SystemBuffer;
+
     switch (irpSp->Parameters.DeviceIoControl.IoControlCode) {
+    case Ioctls::LIST_FIXTURES:
+        if (inBuffer || inBufferLength) {
+            return STATUS_INVALID_PARAMETER;
+        }
+        return handleListFixturesIoctl(static_cast<FixtureInfo*>(outBuffer), outBufferLength / sizeof(outBuffer));
+    case Ioctls::LIST_TESTS:
+        break;
+    case Ioctls::RUN_TEST:
+        break;
     default:
         return STATUS_INVALID_DEVICE_REQUEST;
     }
+}
+
+NTSTATUS handleListFixturesIoctl(FixtureInfo* outFixtures, size_t outFixturesLength) {
+    const auto fixtures = FixturesManager::instance().listFixtures();
+    for (uint32_t index = 0; index < min(fixtures.size(), outFixturesLength); index++) {
+        outFixtures[index] = fixtures.at(index);
+    }
+    return (outFixturesLength < fixtures.size()) ? STATUS_MORE_ENTRIES : STATUS_SUCCESS;
+}
+
+NTSTATUS handleListTestsIoctl(PIRP irp) {
+
+}
+
+NTSTATUS handleRunTestIoctl(PIRP irp) {
+
 }
 
 }
