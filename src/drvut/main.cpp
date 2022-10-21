7f5e606ee256fc81220e0cf78445d08d01197f3c
oferda4 <oferda4@gmail.com>
2022-10-21 16:21:47 +0300

    chore: made an ioctl stub

--- src/drvut/main.cpp
+++ src/drvut/main.cpp
@@ -6,10 +6,15 @@
 namespace drvut {
 namespace internal {
 
-static constexpr UNICODE_STRING NT_DEVICE_NAME = RTL_CONSTANT_STRING(L"\\Device\\TestDriver");
-static constexpr UNICODE_STRING DOS_DEVICE_NAME = RTL_CONSTANT_STRING(L"\\DosDevices\\TestDriver");
+namespace {
+constexpr UNICODE_STRING NT_DEVICE_NAME = RTL_CONSTANT_STRING(L"\\Device\\TestDriver");
+constexpr UNICODE_STRING DOS_DEVICE_NAME = RTL_CONSTANT_STRING(L"\\DosDevices\\TestDriver");
 
-static void unloadDriver(PDRIVER_OBJECT DriverObject);
+void unloadDriver(PDRIVER_OBJECT DriverObject);
+NTSTATUS doNothing(PDEVICE_OBJECT deviceObject, PIRP irp);
+NTSTATUS deviceControl(PDEVICE_OBJECT deviceObject, PIRP irp);
+NTSTATUS performIoctl(PIRP irp);
+}
 
 EXTERN_C NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath) {
     UNREFERENCED_PARAMETER(RegistryPath);
@@ -18,7 +23,12 @@ EXTERN_C NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING Regis
     SymbolicLinkGuard symbolicLink(SymbolicLink(&DOS_DEVICE_NAME, &NT_DEVICE_NAME));
 
     TRACE("Driver Entry\n");
+
+    DriverObject->MajorFunction[IRP_MJ_CREATE] = doNothing;
+    DriverObject->MajorFunction[IRP_MJ_CLOSE] = doNothing;
+    DriverObject->MajorFunction[IRP_MJ_DEVICE_CONTROL] = deviceControl;
     DriverObject->DriverUnload = unloadDriver;
+
     CHECK_AND_RETHROW(device.init());
     CHECK_AND_RETHROW(symbolicLink.init());
 
@@ -27,6 +37,8 @@ EXTERN_C NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING Regis
 
     return STATUS_SUCCESS;
 }
+
+namespace {
 
 void unloadDriver(PDRIVER_OBJECT DriverObject) {
     PDEVICE_OBJECT deviceObject = DriverObject->DeviceObject;
@@ -38,6 +50,43 @@ void unloadDriver(PDRIVER_OBJECT DriverObject) {
     FixturesManager::destroy();
     DeviceGuard device(Device(deviceObject), true);
     SymbolicLinkGuard symbolicLink(SymbolicLink(&DOS_DEVICE_NAME), true);
+}
+
+NTSTATUS doNothing(PDEVICE_OBJECT deviceObject, PIRP irp) {
+    UNREFERENCED_PARAMETER(deviceObject);
+
+    irp->IoStatus.Status = STATUS_SUCCESS;
+    irp->IoStatus.Information = 0;
+
+    IoCompleteRequest(irp, IO_NO_INCREMENT);
+
+    return STATUS_SUCCESS;
+}
+
+NTSTATUS deviceControl(PDEVICE_OBJECT deviceObject, PIRP irp) {
+    UNREFERENCED_PARAMETER(deviceObject);
+    const NTSTATUS result = performIoctl(irp);
+
+    irp->IoStatus.Status = result;
+    IoCompleteRequest(irp, IO_NO_INCREMENT);
+    return result;
+}
+
+NTSTATUS performIoctl(PIRP irp) {
+    auto irpSp = IoGetCurrentIrpStackLocation(irp);
+    const auto inBufferLength = irpSp->Parameters.DeviceIoControl.InputBufferLength;
+    const auto outBufferLength = irpSp->Parameters.DeviceIoControl.OutputBufferLength;
+
+    if (!inBufferLength || !outBufferLength) {
+        return STATUS_INVALID_PARAMETER;
+    }
+
+    switch (irpSp->Parameters.DeviceIoControl.IoControlCode) {
+    default:
+        return STATUS_INVALID_DEVICE_REQUEST;
+    }
+}
+
 }
 
 }
