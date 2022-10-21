2b44a2f23791c2f6cdca307cd4c20a00543cd976
oferda4 <oferda4@gmail.com>
2022-10-21 18:16:11 +0300

    chore: define structs for ioctl

--- src/common/Ioctls.h
+++ src/common/Ioctls.h
@@ -4,7 +4,20 @@
 
 class Ioctls {
 public:
-    static constexpr uint32_t GET_FIXTURES = CTL_CODE(FILE_DEVICE_UNKNOWN, 0x800, METHOD_BUFFERED, FILE_READ_DATA);
-    static constexpr uint32_t GET_TESTS = CTL_CODE(FILE_DEVICE_UNKNOWN, 0x801, METHOD_BUFFERED, FILE_READ_DATA);
-    static constexpr uint32_t RUN_TESTS = CTL_CODE(FILE_DEVICE_UNKNOWN, 0x802, METHOD_BUFFERED, FILE_WRITE_DATA);
+    struct ListTestsInput {
+        uint32_t fixtureId;
+    };
+
+    struct RunTestInput {
+        uint32_t fixtureId;
+        uint32_t testId;
+    };
+
+    struct RunTestOutput {
+        NTSTATUS result;
+    };
+
+    static constexpr uint32_t LIST_FIXTURES = CTL_CODE(FILE_DEVICE_UNKNOWN, 0x800, METHOD_BUFFERED, FILE_READ_DATA);
+    static constexpr uint32_t LIST_TESTS = CTL_CODE(FILE_DEVICE_UNKNOWN, 0x801, METHOD_BUFFERED, FILE_READ_DATA);
+    static constexpr uint32_t RUN_TEST = CTL_CODE(FILE_DEVICE_UNKNOWN, 0x802, METHOD_BUFFERED, FILE_WRITE_DATA);
 };
