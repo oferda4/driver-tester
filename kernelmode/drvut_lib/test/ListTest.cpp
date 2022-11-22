13de5bb5d227d55d929d769b075e0fccaa80fd04
oferda4 <oferda4@gmail.com>
2022-11-22 12:55:39 +0200

    test: ListUtils

--- kernelmode/drvut_lib/test/ListTest.cpp
+++ kernelmode/drvut_lib/test/ListTest.cpp
@@ -23,6 +23,7 @@ struct LeakCounter {
 uint32_t LeakCounter::leaked = 0;
 
 void insertAndAssert(List<int>& list, ::std::list<int>& prototype, int value);
+const auto trivialCompare = [](uint32_t a, uint32_t b) { return a == b; };
 }
 
 TEST(ListTest, Functionality) {
@@ -48,6 +49,36 @@ TEST(ListTest, Dtor) {
         ASSERT_EQ(LeakCounter::leaked, arbitraryNumberOfValues);
     }
     ASSERT_EQ(LeakCounter::leaked, 0);
+}
+
+TEST(ListUtilsTest, FindExistingElement) {
+    List<uint32_t> list;
+    list.push_back(2);
+    list.push_back(230);
+    list.push_back(8);
+    list.push_back(1001);
+
+    const uint32_t arbitraryExistingElement = 8;
+    auto result = ListUtils::find(list, arbitraryExistingElement, trivialCompare);
+    const auto constResult = ListUtils::find(static_cast<const List<uint32_t>&>(list), arbitraryExistingElement, trivialCompare);
+    ASSERT_TRUE(result != nullptr);
+    ASSERT_EQ(*result, arbitraryExistingElement);
+    ASSERT_TRUE(constResult != nullptr);
+    ASSERT_EQ(*constResult, arbitraryExistingElement);
+}
+
+TEST(ListUtilsTest, FindNonExistingElement) {
+    List<uint32_t> list;
+    list.push_back(0);
+    list.push_back(5);
+    list.push_back(2);
+    list.push_back(1);
+
+    const uint32_t arbitraryNonExistingElement = 11;
+    auto result = ListUtils::find(list, arbitraryNonExistingElement, trivialCompare);
+    const auto constResult = ListUtils::find(static_cast<const List<uint32_t>&>(list), arbitraryNonExistingElement, trivialCompare);
+    ASSERT_TRUE(result == nullptr);
+    ASSERT_TRUE(constResult == nullptr);
 }
 
 namespace {
