ded9c30d22a1265d1405943fa81469c0d91f779d
oferda4 <oferda4@gmail.com>
2022-12-22 13:38:18 +0200

    feat: a simple connection over stream

---
+++ runner/runner/src/Connection.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace runner {
+    interface Connection {
+        void send(byte[] data);
+        byte[] recv();
+    }
+
+    class ConnectionImpl : Connection {
+        const int HEADER_SIZE = 4;
+
+        private Stream stream;
+
+        public ConnectionImpl(Stream stream) {
+            this.stream = stream;
+        }
+
+        public byte[] recv() {
+            int size = BitConverter.ToInt32(recvAll(HEADER_SIZE), 0);
+            return recvAll(size);
+        }
+
+        private byte[] recvAll(int size) {
+            int bytesRecv = 0;
+            byte[] buffer = new byte[size];
+
+            while (bytesRecv < size) {
+                int currBytesRecv = stream.Read(buffer, bytesRecv, size - bytesRecv);
+                if (currBytesRecv == 0) {
+                    // TODO: throw
+                }
+                bytesRecv += currBytesRecv;
+            }
+
+            return buffer;
+        }
+
+        public void send(byte[] data) {
+            sendAll(BitConverter.GetBytes(data.Length));
+            sendAll(data);
+        }
+
+        private void sendAll(byte[] buffer) {
+            stream.Write(buffer, 0, buffer.Length);
+        }
+    }
+}
---
+++ runner/runner/src/Runner.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace runner {
+    public class Runner {
+        public Runner() { 
+        }
+
+        public Queue<uint> listTests() {
+            throw new NotImplementedException();
+        }
+
+        public uint runTest(uint testId) {
+            throw new NotImplementedException();
+        }
+    }
+}
