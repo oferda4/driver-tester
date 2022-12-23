847e1c7533be713c7b1b3957e1bad230a6cb9a5a
oferda4 <oferda4@gmail.com>
2022-12-23 13:29:27 +0200

    chore: add serialization interface

---
+++ runner/runner/src/InternalMessages.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Runner {
+    public static class InternalMessages {
+        public struct TestInfo {
+            uint id;
+            string name;
+        };
+
+        public struct TestResult {
+            int status;
+        };
+
+        public struct ListTestsInput {
+            // intentionally left blank
+        };
+
+        public struct ListTestsOutput {
+            List<TestInfo> tests;
+        };
+
+        public struct RunTestInput {
+            uint testId;
+        };
+
+        public struct RunTestOutput {
+            TestResult result;
+        };
+    }
+}
---
+++ runner/runner/src/Serializer.cs
@@ -0,0 +1,15 @@
+﻿using Runner;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Runner {
+    public interface Serializer {
+        byte[] serializeListTestsInput(InternalMessages.ListTestsInput input);
+        byte[] serializeRunTestInput(InternalMessages.RunTestInput input);
+        InternalMessages.ListTestsOutput deserializeListTestsOutput(byte[] output);
+        InternalMessages.RunTestOutput deserializeRunTestOutput(byte[] output);
+    }
+}
