acabee110c64ae37045b5267888d48c56038d052
oferda4 <oferda4@gmail.com>
2022-12-23 13:39:49 +0200

    rename: Serializer -> Parser

--- runner/runner/src/Parser.cs
+++ runner/runner/src/Parser.cs
@@ -6,10 +6,10 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Runner {
-    public interface Serializer {
+    public interface Parser {
         byte[] serializeListTestsInput(InternalMessages.ListTestsInput input);
         byte[] serializeRunTestInput(InternalMessages.RunTestInput input);
-        InternalMessages.ListTestsOutput deserializeListTestsOutput(byte[] output);
-        InternalMessages.RunTestOutput deserializeRunTestOutput(byte[] output);
+        InternalMessages.ListTestsOutput parseListTestsOutput(byte[] output);
+        InternalMessages.RunTestOutput parseRunTestOutput(byte[] output);
     }
 }
