348a5bd9aa3d6150a9808b033f790e531776fa4a
oferda4 <oferda4@gmail.com>
2022-12-24 16:47:05 +0200

    chore: make a basic exe for runner

---
+++ runner/runner_exe/Main.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Runner {
+    internal class RunnerMain {
+        static int Main(string[] args) {
+            if (args.Length != 3) {
+                printUsages();
+                return 1;
+            }
+
+            Console.WriteLine("Connecting to server");
+            TcpClient client = new TcpClient(args[1], int.Parse(args[2]));
+
+            client.GetStream().Close();
+            client.Close();
+
+            pause();
+            return 0;
+        }
+
+        static void printUsages() {
+            Console.WriteLine("Usages: runner.exe <ip> <port>");
+        }
+
+        static void pause() {
+            Console.ReadKey();
+        }
+    }
+}
