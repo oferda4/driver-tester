47a8ecadf89b1620faaa5e726c2c29ae31e3bae7
oferda4 <oferda4@gmail.com>
2022-12-31 16:01:04 +0200

    feat: protobuf parser

---
+++ runner/runner/src/ProtobufParser.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Protobuf;
+
+namespace Runner {
+    public class ProtobufParser : Parser {
+        public InternalMessages.ListTestsOutput parseListTestsOutput(byte[] output) {
+            var response = ListTestsResponse.Parser.ParseFrom(output);
+            var testsInfo = response.Tests
+                .Select(info => new InternalMessages.TestInfo(info.Id, info.Name))
+                .ToList();
+            return new InternalMessages.ListTestsOutput(testsInfo);
+        }
+
+        public InternalMessages.RunTestOutput parseRunTestOutput(byte[] output) {
+            var response = RunTestResponse.Parser.ParseFrom(output);
+            return new InternalMessages.RunTestOutput(new InternalMessages.TestResult(response.Status));
+        }
+
+        public byte[] serializeListTestsInput(InternalMessages.ListTestsInput input) {
+            // currently there is no input
+            var listTestsRequest = new ListTestsRequest();
+            return listTestsRequest.ToByteArray();
+        }
+
+        public byte[] serializeRunTestInput(InternalMessages.RunTestInput input) {
+            var runTestRequest = new RunTestRequest() { TestId=input.testId };
+            return runTestRequest.ToByteArray();
+        }
+    }
+}
---
+++ runner/runner/test/ProtobufParserTest.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Runner;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Protobuf;
+
+namespace Runner.Tests {
+    [TestClass()]
+    public class ProtobufParserTest {
+        [TestMethod()]
+        public void parseListTestsOutputTest() {
+            var fakeTests = new List<InternalMessages.TestInfo> {
+                new InternalMessages.TestInfo(0, "Frst Test"),
+                new InternalMessages.TestInfo(4, "Some Test"),
+                new InternalMessages.TestInfo(5, "ASDASD"),
+                new InternalMessages.TestInfo(12, "!!!@@@####")
+            };
+
+            var fakeResponse = new ListTestsResponse();
+            foreach (var test in fakeTests) {
+                fakeResponse.Tests.Add(new ListTestsResponse.Types.TestInfo() { Id = test.id, Name = test.name });
+            }
+
+            var result = (new ProtobufParser()).parseListTestsOutput(fakeResponse.ToByteArray());
+            Assert.AreEqual(result.tests.Count, fakeTests.Count);
+            for (int i = 0; i < result.tests.Count; i++) {
+                Assert.AreEqual(result.tests[i].id, fakeTests[i].id);
+                Assert.AreEqual(result.tests[i].name, fakeTests[i].name);
+            }
+        }
+
+        [TestMethod()]
+        public void parseRunTestOutputTest() {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void serializeListTestsInputTest() {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void serializeRunTestInputTest() {
+            Assert.Fail();
+        }
+    }
+}
