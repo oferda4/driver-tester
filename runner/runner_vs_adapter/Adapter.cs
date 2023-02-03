886512a4f23bde6f9da663fd208a599a854d4774
oferda4 <oferda4@gmail.com>
2023-02-03 12:55:17 +0200

    feat: template for the vs adapter

---
+++ runner/runner_vs_adapter/Adapter.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
+
+namespace RunnerAdapter {
+
+[ExtensionUri("executor://RunnerExecutor/v1")]
+public class RunnerExecutor : ITestExecutor {
+    public void Cancel() {
+        throw new NotImplementedException();
+    }
+
+    public void RunTests(IEnumerable<TestCase>? tests, IRunContext? runContext, IFrameworkHandle? frameworkHandle) {
+        throw new NotImplementedException();
+    }
+
+    public void RunTests(IEnumerable<string>? sources, IRunContext? runContext, IFrameworkHandle? frameworkHandle) {
+        throw new NotImplementedException();
+    }
+}
+
+[ExtensionUri("discovery://RunnerDiscoverer/v1")]
+public class RunnerDiscoverer : ITestDiscoverer {
+    public void DiscoverTests(IEnumerable<string> sources, IDiscoveryContext discoveryContext, IMessageLogger logger, ITestCaseDiscoverySink discoverySink) {
+        throw new NotImplementedException();
+    }
+}
+
+}
