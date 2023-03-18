02f45cc2ee75cd33cf5789300ac1e36c3f81e6e6
oferda4 <oferda4@gmail.com>
2023-03-18 20:27:48 +0200

    feat: find machine ip automatically

--- runner/runner_exe/Main.cs
+++ runner/runner_exe/Main.cs
@@ -1,17 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
 
 namespace Runner {
     internal class RunnerMain {
         private const int SERVER_PORT = 33333;
-        private const string SERVER_IP_ENVIRONMENT_VARABLE_NAME = "TEST_MACHINE_IP";
+        private const string VM_NAME_ENVIRONMENT_VARIABLE_NAME = "TEST_MACHINE_NAME";
 
         static int Main(string[] args) {
-            TcpClient client = new TcpClient(Environment.GetEnvironmentVariable(SERVER_IP_ENVIRONMENT_VARABLE_NAME), 
+            var ip = getVirtualMachineIP();
+            TcpClient client = new TcpClient(getVirtualMachineIP(), 
                                              SERVER_PORT);
 
             try {
@@ -47,6 +52,30 @@ namespace Runner {
 
         static void pause() {
             Console.ReadKey();
+        }
+
+        static string getVirtualMachineIP() {
+            string vmName = Environment.GetEnvironmentVariable(VM_NAME_ENVIRONMENT_VARIABLE_NAME);
+            string script = string.Format("Get-VMNetworkAdapter -VMName {0} | Select-Object -ExpandProperty IPAddresses", vmName);
+            return runPowerShellScript(script);
+        }
+
+        static string runPowerShellScript(string script) {
+            Runspace runspace = RunspaceFactory.CreateRunspace();
+            runspace.Open();
+
+            Pipeline pipeline = runspace.CreatePipeline();
+            pipeline.Commands.AddScript(script);
+
+            Collection<PSObject> results = pipeline.Invoke();
+            runspace.Close();
+
+            if (results == null || results.Count == 0) {
+                throw new Exception("no ip found");
+            }
+
+            // TODO: find the proper ip that is connected to the host if multiple ips found.
+            return results[0].ToString();
         }
     }
 }
